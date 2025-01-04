671459b5fc32317c221a635e5d2acb8d5f1310f4
Wangxixigit <widiyantiintan07@gmail.com>
2025-01-04 19:44:32 +0700

    hero

--- resources/views/layouts/landing/hero.blade.php
+++ resources/views/layouts/landing/hero.blade.php
@@ -3,7 +3,7 @@
         <div class="grid grid-cols-1 md:grid-cols-12 gap-4 items-center">
             <div class="md:col-span-7 flex flex-col gap-4">
                 <h1 class="text-center md:text-start text-4xl font-bold md:text-6xl text-gray-100">
-                    Gudangku
+                    Welcome my stock!
                 </h1>
                 <p class="text-sm md:text-lg text-center md:text-start text-gray-300">
                     Gudangku membantu memberikan informasi barang digudang secara menyeluruh dan lengkap serta
