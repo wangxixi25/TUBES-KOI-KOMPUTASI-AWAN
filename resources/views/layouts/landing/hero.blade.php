212618a91cc42538ae2bdbae31ed28c1ace21898
Wangxixigit <widiyantiintan07@gmail.com>
2025-01-04 20:15:02 +0700

    hero

--- resources/views/layouts/landing/hero.blade.php
+++ resources/views/layouts/landing/hero.blade.php
@@ -1,13 +1,12 @@
-<div class="text-white bg-sky-800 w-full py-10">
+<div class="text-white w-full py-10" style="background-color: #2c386c;">
     <div class="container mx-auto px-4">
         <div class="grid grid-cols-1 md:grid-cols-12 gap-4 items-center">
             <div class="md:col-span-7 flex flex-col gap-4">
                 <h1 class="text-center md:text-start text-4xl font-bold md:text-6xl text-gray-100">
-                    Welcome my stock!
+                    MyStock Good One Koi!
                 </h1>
                 <p class="text-sm md:text-lg text-center md:text-start text-gray-300">
-                    Gudangku membantu memberikan informasi barang digudang secara menyeluruh dan lengkap serta
-                    permintaan barang dapat dilakukan dimana saja.
+                    Menyediakan informasi stok ikan Koi secara lengkap dan akurat, sehingga dapat mengelola inventaris yang lebih efisien dan permintaan barang dapat dilakukan kapan saja, di mana saja.
                 </p>
             </div>
             <div class="hidden md:flex md:col-span-5">
